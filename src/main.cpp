3615c54eb9bfe39128d7ffc94d2c41aa496c4bd3
Grzegorz Kraszewski <grzegorz.kraszewski@gmail.com>
2024-09-17 21:20:51 +0200

    Bzted now reads input file name from the commandline. Output file name is a commandline argument now as well. If output file name is not specified, it is generated by removing ".bzt" extension if exists and appedning ".asm".

--- src/main.cpp
+++ src/main.cpp
@@ -11,22 +11,71 @@ extern Library *SysBase, *DOSBase;
 Logger log;
 Compiler *Comp;
 
-/*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------------------------
+// Removes ".bzt" extension from the input name (if exists) and appends ".asm" extenstion
+
+static char* createOutputName(const char *input)
+{
+	char *output;
+	int inputLength = StrLen(input);
+
+	if (StrCmp(input + inputLength - 4, ".bzt") == 0)
+	{
+		if (output = StrClone(input))
+		{
+			StrCopy(".asm", output + inputLength - 4);
+		}
+	}
+	else
+	{
+		if (output = new char[inputLength + 5])
+		{
+			StrCopy(".asm", StrCopy(input, output));
+		}
+	}
+	
+	return output;
+}
+
+
+//-----------------------------------------------------------------------------
 
 LONG Main(WBStartup *wbmsg)
 {
 	Compiler compiler;
 	Comp = &compiler;
 	wbmsg = wbmsg;
+	RDArgs *args;
+	LONG argArray[2] = { 0, 0 };
 
+	wbmsg = wbmsg;
 	log.setLevel(LOGLEVEL_DEBUG);
-	compiler.scan("test.bzt");
-	compiler.lex();
-	compiler.dumpTokens();
-	compiler.translate();
-	compiler.transform();
-	compiler.dumpFunctions();
-	compiler.optimize();
-	compiler.dumpFunctions();
+
+	if (args = ReadArgs("FROM/A,TO", argArray, NULL))
+	{
+		char *inputName, *outputName;
+	
+		inputName = (char*)argArray[0];
+		outputName = (char*)argArray[1];
+
+		if (!outputName) outputName = createOutputName(inputName);
+
+		if (outputName)
+		{
+			compiler.scan(inputName);
+			compiler.lex();
+			compiler.dumpTokens();
+			compiler.translate();
+			compiler.transform();
+			compiler.dumpFunctions();
+			compiler.optimize();
+			compiler.dumpFunctions();
+		}
+
+		FreeArgs(args);
+	}
+	else PrintFault(IoErr(), "bzted: arguments");
+
 	return 0;
 }
